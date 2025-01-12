7db9e7faed04c948595b3a3035362acb0fe2fcd1
Nicholas Perry <nuperry2011@gmail.com>
2025-01-12 14:39:47 -0700

    updating changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['localhost'],
+  },
   reactStrictMode: true,
   swcMinify: true,
   env: {
