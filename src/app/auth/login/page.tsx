16fe283f9b1ec514a5dac5334738df2057cec058
Nicholas Perry <nuperry2011@gmail.com>
2025-01-12 02:47:49 -0700

    update

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -134,7 +134,7 @@ export default function Login() {
         <div className="text-sm text-center mt-4 space-y-2">
           <div>
             <Link href="/auth/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
-              Don't have an account? Sign up
+              Don&apos;t have an account? Sign up
             </Link>
           </div>
           <div>
